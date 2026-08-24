5a84b712cf6a9d0ccf36a9a3d7c868d80d02e9a7
Takhellambam Abinanda Singh <seityabari2017@gmail.com>
2026-08-24 14:35:59 +0530

    Set up Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +14,7 @@ function App() {
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
+          <h1 className="text-3xl font-bold text-red-500">Get started</h1>
         </div>
         <div>
           <h1>Get started</h1>
